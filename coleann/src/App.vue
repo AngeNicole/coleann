1826d51f34359dba32489dc780ba8bfd68edd6c9
AngeNicole <angenicolemukundwa22@gmail.com>
2021-06-01 22:09:33 +0200

    Updating navbar

--- coleann/src/App.vue
+++ coleann/src/App.vue
@@ -1,66 +1,91 @@
 <template>
-<div>
-    <Nav :navLinks="navLinks" :navConfig="navConfig" :btnConfig="btnConfig">
-      <img class="img" src="" alt="" srcset="" /> 
-    </Nav>
-  </div>
+    <vue-navigation-bar :options="options" />
 </template>
+
 <script>
-import VueNavUI from "vue-nav-ui";
-import { defineComponent, ref } from "vue";
-export default defineComponent({
-  name: 'home',
-  components: {
-    VueNavUI
-  },
-  setup(){
-    const navLinks = ref([
-      {
-        name: "Home",
-        path: "/",
-      },
-      {
-        name: "About",
-        path: "/about",
-      },
-      {
-        name: "FAQs",
-        path: "/faqs",
-      },
-      {
-        name: "More",
-        path: "/more",
-      },
-      {
-        name: "Media",
-        path: "/media",
-      }
-    ]);
+    
+</script>
 
-    const navConfig = ref({
-      whitespace: true,
-      navBg: "#FAFAFA",
-      navBorderRadius: "30px",
-      linkFont: "poppins",
-      linkColor: "black",
-      responsivePosition: "top or bottom"
-    });
+<style lang="scss">
+    .vnb {
+        .button-red {
+            background: #FF3B30;
 
-    const btnConfig = ref({
-      btnLink: true,
-      btnUrl: "https://dhaniel.disha.page",
-      btnText: "Download app",
-      btnBg: "#40269E",
-      btnTextColor: "White",
-      btnBorderWidth: "0",
-      btnBorderColor: "Black",
-      btnBorderRadius: "20px",
-    });
+            &:hover {
+                background: darken(#FF3B30, 10%);
+            }
+        }
+    }
+</style>
 
-    return {navLinks, btnConfig, navConfig};
-  }
-});
+<script src="https://unpkg.com/vue-navigation-bar@1.0.0/dist/vue-navigation-bar.min.js"></script>
+
+
+<script>
+
+
+// import VueNavUI from "vue-nav-ui";
+
+// import { defineComponent, ref } from "vue";
+// export default defineComponent({
+//   name: 'home',
+//   components: {
+//     VueNavUI
+//   },
+//   setup(){
+//     const navLinks = ref([
+//       {
+//         name: "Home",
+//         path: "/",
+//       },
+//       {
+//         name: "About",
+//         path: "/about",
+//       },
+//       {
+//         name: "FAQs",
+//         path: "/faqs",
+//       },
+//       {
+//         name: "More",
+//         path: "/more",
+//       },
+//       {
+//         name: "Media",
+//         path: "/media",
+//       }
+//     ]);
+
+//     const navConfig = ref({
+//       whitespace: true,
+//       navBg: "#FAFAFA",
+//       navBorderRadius: "30px",
+//       linkFont: "poppins",
+//       linkColor: "black",
+//       responsivePosition: "top or bottom"
+//     });
+
+//     const btnConfig = ref({
+//       btnLink: true,
+//       btnUrl: "https://dhaniel.disha.page",
+//       btnText: "Download app",
+//       btnBg: "#40269E",
+//       btnTextColor: "White",
+//       btnBorderWidth: "0",
+//       btnBorderColor: "Black",
+//       btnBorderRadius: "20px",
+//     });
+
+//     return {navLinks, btnConfig, navConfig};
+//   }
+// });
+
+
+
 </script>
 <style lang="scss" scoped>
+
   
   </style>
+  <script src="https://unpkg.com/vue-navigation-bar@1.0.0/dist/vue-navigation-bar.min.js"></script>
+
