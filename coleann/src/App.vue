55130906b05ad62a99a7cf5efb2617b1c20a72ae
AngeNicole <angenicolemukundwa22@gmail.com>
2021-06-08 21:05:10 +0200

    Initial Home page

--- coleann/src/App.vue
+++ coleann/src/App.vue
@@ -1,6 +1,81 @@
   <template>
   <div id="app">
-    <div class="center example-nav">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+                >Disabled</a
+              >
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <!-- <div id="app">
+    <div class="navbar">
+      <a href="#">Home</a>
+      <a href="#">Story</a>
+      <a href="#">Blog</a>
+      <a href="#">FAQs</a>
+    </div> -->
+
+  <!-- <div class="center example-nav">
       <input class="input-color" v-model="colorx" type="color" name="" id="">
       
       <vs-navbar not-line center-collapsed v-model="active" class="navbar"  :color="colorx"
@@ -32,25 +107,24 @@
         </template>
       </vs-navbar>
       
-    </div>
-    </div>
-  </template>
+    </div> -->
+</template>
 
  <script>
-  export default {
-    data:() => ({
-      active: 0,
 
-      colorx:'#BFCBE3'
-    })
-  }
-  </script>
-<style lang="stylus">
- .input-color
-  position relative
-  display inline-block
- .navbar
-  height 50px
-  font 20px
-  
+export default {
+  data: () => ({
+    active: 0,
+
+    colorx: "#BFCBE3",
+  }),
+};
+</script>
+<style>
+.navbar {
+  background-color: black;
+  height: 80px;
+  font-family: "TW cent MT";
+}
 </style>
+
--- coleann/src/main.js
+++ coleann/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuesax from 'vuesax'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import 'vuesax/dist/vuesax.css' //Vuesax styles
 Vue.use(Vuesax, {
@@ -10,6 +13,8 @@ Vue.use(Vuesax, {
 // import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
 
